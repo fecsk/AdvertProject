274bc8d58439f728727bc09316c4af33208fb0ff
szabohunor96 <30519476+szabohunor96@users.noreply.github.com>
2017-12-07 13:38:37 +0200

    2 branch

--- app/src/main/java/com/example/asus/advertproject/advertfeed/DetailedAdvertFragment.java
+++ app/src/main/java/com/example/asus/advertproject/advertfeed/DetailedAdvertFragment.java
@@ -1,11 +1,41 @@
 package com.example.asus.advertproject.advertfeed;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.asus.advertproject.R;
 
 /**
  * Created by Asus on 2017. 11. 08..
  */
 
-public class DetailedAdvertFragment  extends Fragment {
+public class DetailedAdvertFragment  extends Fragment{
+    private View view;
+    public DetailedAdvertFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        }
+        try {
+            view = inflater.inflate(R.layout.advertfeed_fragment, container, false);
+        } catch (InflateException e) {
+            e.printStackTrace();
+        }
+
+
+        return view;
+    }
 }
--- app/src/main/java/com/example/asus/advertproject/main/MainActivity.java
+++ app/src/main/java/com/example/asus/advertproject/main/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.asus.advertproject.main;
 
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 import com.example.asus.advertproject.R;
 import com.example.asus.advertproject.advertfeed.*;
+import com.example.asus.advertproject.login.LoginActivity;
+import com.example.asus.advertproject.login.RegisterActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +29,23 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     private Toolbar toolbar;
     private NavigationManager manager;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.appbar_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorAction));
+        }
+
 
         initViews();
+       /* for(int i=0;i<10;i++) {
+            sendPostToFirebase();
+        }
+        */
         manager.switchToMainFragment(new AdvertFeedFragment());
     }
 
@@ -31,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         manager = new NavigationManager(getSupportFragmentManager());
         toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         bottomNavigationView = findViewById(R.id.main_navigation);
 
         BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -64,6 +86,48 @@ public class MainActivity extends AppCompatActivity {
     public void setSelectedMenuItem(int resId){
         bottomNavigationView.setSelectedItemId(resId);
     }
+
+    private void sendPostToFirebase() {
+
+        String timestamp = Long.toString(System.currentTimeMillis());
+        Advert advert=new Advert("ABCD!!","nagyon finom erett banan 10 lej/kg vagy cserelem kukoricara 1:10 aranyban","abc",
+                timestamp,"433543543","gs://advertproject-10f39.appspot.com/advertpics/banana.jpg","gs://advertproject-10f39.appspot.com/profilepics/harambe.jpg");
+        String key = mDatabase.child("adverts").push().getKey();
+        mDatabase.child("adverts").child(key).setValue(advert);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        if (id == R.id.Addbutton) {
+            Intent i=new Intent(MainActivity.this,AddActivity.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.Loginbutton) {
+            Intent i=new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
 
