8358d1d74576a1409886c8e842f8ec864929a545
szabohunor96 <30519476+szabohunor96@users.noreply.github.com>
2017-12-23 22:12:49 +0200

    proba commit

---
+++ app/src/main/java/com/example/asus/advertproject/advertfeed/ViewPagerAdapter.java
@@ -0,0 +1,10 @@
+package com.example.asus.advertproject.advertfeed;
+
+import android.support.v4.view.PagerAdapter;
+
+/**
+ * Created by Asus on 2017. 12. 23..
+ */
+
+public abstract class ViewPagerAdapter extends PagerAdapter {
+}
