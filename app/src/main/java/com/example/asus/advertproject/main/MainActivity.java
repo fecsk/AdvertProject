72d75767cc7e5cae5c3bf592c8660cf72b39d9af
fecsk <33343018+fecsk@users.noreply.github.com>
2018-01-02 11:45:18 +0200

    fixed login/logout from main, added more strings

--- app/src/main/java/com/example/asus/advertproject/main/MainActivity.java
+++ app/src/main/java/com/example/asus/advertproject/main/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.asus.advertproject.main;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -8,15 +10,20 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.asus.advertproject.R;
 import com.example.asus.advertproject.advertfeed.*;
 import com.example.asus.advertproject.login.LoginActivity;
 import com.example.asus.advertproject.model.Advert;
 import com.example.asus.advertproject.profile.ViewProfileActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -27,11 +34,16 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private NavigationManager manager;
     private DatabaseReference mDatabase;
+    private FirebaseAuth auth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.appbar_main);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             getWindow().setStatusBarColor(getResources().getColor(R.color.colorAction));
@@ -43,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             sendPostToFirebase();
         }
         */
+
         manager.switchToMainFragment(new AdvertFeedFragment());
     }
 
@@ -73,12 +86,8 @@ public class MainActivity extends AppCompatActivity {
         };
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+
     }
-
-
-
-
-
 
 
     public void setSelectedMenuItem(int resId){
@@ -100,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        user = auth.getCurrentUser();
         //noinspection SimplifiableIfStatement
 
         if (id == R.id.Addbutton) {
@@ -109,14 +118,44 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         if (id == R.id.Loginbutton) {
-            Intent i=new Intent(MainActivity.this,LoginActivity.class);
-            startActivity(i);
-            return true;
+            if(user == null) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                return true;
+            }
+            else{
+                AlertDialog.Builder builder;
+                builder = new AlertDialog.Builder(MainActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+                builder.setTitle("Logout")
+                        .setMessage("Are you sure you want to log out?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                FirebaseAuth.getInstance().signOut();
+
+                                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(i);
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
         }
         if (id == R.id.viewProfileButton) {
-            Intent i=new Intent(MainActivity.this,ViewProfileActivity.class);
-            startActivity(i);
-            return true;
+            if(user != null) {
+                Intent i = new Intent(MainActivity.this, ViewProfileActivity.class);
+                startActivity(i);
+                return true;
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Please log in first!",
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
         }
         if (id == R.id.editProfileButton) {
             Intent i=new Intent(MainActivity.this,ViewProfileActivity.class);
