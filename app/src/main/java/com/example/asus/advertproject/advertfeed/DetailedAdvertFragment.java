89912b73a368e8f4d7a6088737531fe938fe96e6
szabohunor96 <30519476+szabohunor96@users.noreply.github.com>
2018-01-02 20:03:02 +0200

    pageview + design added

--- app/src/main/java/com/example/asus/advertproject/advertfeed/DetailedAdvertFragment.java
+++ app/src/main/java/com/example/asus/advertproject/advertfeed/DetailedAdvertFragment.java
@@ -3,6 +3,7 @@ package com.example.asus.advertproject.advertfeed;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +24,8 @@ import com.google.firebase.storage.StorageReference;
 
 public class DetailedAdvertFragment  extends Fragment{
     private View view;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
     public DetailedAdvertFragment() {
         // Required empty public constructor
     }
@@ -42,18 +45,20 @@ public class DetailedAdvertFragment  extends Fragment{
             e.printStackTrace();
         }
 
+
+
         Advert advert = (Advert) getArguments().get("advert");
-        ImageView image = (ImageView)view.findViewById(R.id.imageView);
+
         TextView title=(TextView) view.findViewById(R.id.titleTV);
         TextView descript=(TextView) view.findViewById(R.id.descriptionTV);
         title.setText(advert.getTitle());
         descript.setText(advert.getDescription());
         StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(advert.getPhotoURL());
 
-        Glide.with(this)
-                .using(new FirebaseImageLoader())
-                .load(storageReference)
-                .into(image );
+        viewPager=(ViewPager) view.findViewById(R.id.viewPager);
+        String[] photos=advert.photos.toArray(new String[advert.photos.size()]);
+        viewPagerAdapter=new ViewPagerAdapter(this,photos);
+        viewPager.setAdapter(viewPagerAdapter);
 
 
         return view;
--- app/src/main/java/com/example/asus/advertproject/advertfeed/ViewPagerAdapter.java
+++ app/src/main/java/com/example/asus/advertproject/advertfeed/ViewPagerAdapter.java
@@ -1,11 +1,74 @@
 package com.example.asus.advertproject.advertfeed;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.asus.advertproject.R;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 
 /**
  * Created by Asus on 2017. 12. 23..
  */
 
-public abstract class ViewPagerAdapter extends PagerAdapter {
-    int a=12;
+public class ViewPagerAdapter extends PagerAdapter {
+    Fragment activity;
+    String[] images;
+    LayoutInflater inflater;
+
+    public ViewPagerAdapter(Fragment activity, String[] images)
+    {
+        this.activity=activity;
+        this.images=images;
+    }
+    @Override
+    public int getCount()
+    {
+        return images.length;
+    }
+    @Override
+    public boolean isViewFromObject(View view, Object object)
+    {
+        return view==object;
+    }
+    @Override
+    public Object instantiateItem(ViewGroup container,int position)
+    {
+        inflater=(LayoutInflater) activity.getActivity().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView=inflater.inflate(R.layout.viewpager_item,container,false);
+        ImageView image;
+        image=(ImageView) itemView.findViewById(R.id.imageView2);
+        DisplayMetrics dis=new DisplayMetrics();
+        activity.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dis);
+        int height=dis.heightPixels;
+        int width=dis.widthPixels;
+        image.setMinimumHeight(height);
+        image.setMinimumWidth(width);
+        try {
+            Glide.with(activity.getActivity().getApplicationContext())
+                    .load(images[position])
+                    .into(image);
+        }
+        catch (Exception e)
+        {
+        }
+
+        container.addView(itemView);
+        return itemView;
+
+
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((View)object);
+    }
 }
